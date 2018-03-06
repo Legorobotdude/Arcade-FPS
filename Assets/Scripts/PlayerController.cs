7d06dcba9f2d4a082d45c92dec614df19381fc2f
Aditya Bawankule <adityabawankule@gmail.com>
2018-03-05 20:50:24 -0600

    Write player movement scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
 
-public class PlayerController : MonoBehaviour {
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float lookSensitivity = 3f;
+    [SerializeField] private float jump_force = 500f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private PlayerMotor motor;
+    private CapsuleCollider playerCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        playerCollider = GetComponent<CapsuleCollider>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculate movement vel as 3d vector
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMovement;
+        Vector3 _movVertical = transform.forward * _zMovement;
+
+        Vector3 _velocity;
+
+        if (Input.GetKey(KeyCode.LeftShift) && is_grounded())
+        {
+
+            _velocity = (_movHorizontal + _movVertical).normalized * runSpeed;
+        }
+        else
+        {
+            _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        }
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotation = _xRot * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+
+        Vector3 _thrusterForce = Vector3.zero;
+
+
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && is_grounded())
+        {
+            motor.handle_jump(jump_force);
+        }
+
+    }
+    private bool is_grounded()
+    {
+        float distance_to_ground = playerCollider.bounds.extents.y;
+        return Physics.Raycast(transform.position, -Vector3.up, distance_to_ground + 0.25f);
+    }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -1,16 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMotor : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class PlayerMotor : MonoBehaviour
+{
+
+    [SerializeField] private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private float cameraRotationX = 0f;
+    private float currentCameraRotationX = 0f;
+    private float jump = 0f;
+    private float cameraRotationLimit = 85f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+
+    }
+
+    public void handle_jump(float jump_force)
+    {
+        jump = jump_force;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            //cam.transform.Rotate(-cameraRotation);
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        }
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+        if (jump != 0f)
+        {
+            rb.AddForce(Vector3.up * jump);
+            jump = 0f;
+        }
+
+    }
+
+    public void RotateCamera(float _cameraRotationX)
+    {
+        cameraRotationX = _cameraRotationX;
+    }
 }
