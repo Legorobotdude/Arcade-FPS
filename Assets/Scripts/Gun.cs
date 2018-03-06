29eede7edf9f694e7b84a42cc1a82b5ad666d4cf
Aditya Bawankule <adityabawankule@gmail.com>
2018-03-05 21:49:03 -0600

    Add laser shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 0f;
+
+    public float laserForce = 100f;
+
+    public GameObject muzzle;
+    public ParticleSystem muzzleFlash;
+    public GameObject laserPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +22,18 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+
+
 	}
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(laserPrefab,muzzle.transform.position,muzzle.transform.rotation);
+        laser.GetComponent<Rigidbody>().AddForce(-laser.transform.up * 100f);
+    }
 }
