3be782cadd5e6225e61a9092a1884d94edc6e35a
Aditya Bawankule <adityabawankule@gmail.com>
2018-03-08 21:24:43 -0600

    Move update to gun class

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -86,4 +86,37 @@ public abstract class Gun : MonoBehaviour {
 
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && fireMode == 2)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+        else if (Input.GetButtonDown("Fire1") && fireMode == 0)
+        {
+            Shoot();
+        }
+        else if (Input.GetButton("Fire1") && burstCounter < burstAmount && Time.time >= nextTimeToFire && fireMode == 1)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+            burstCounter++;
+        }
+        else if (Input.GetButtonUp("Fire1") && burstCounter >= burstAmount && fireMode == 1)
+        {
+            burstCounter = 0;
+        }
+
+        if (Input.GetButtonDown("ToggleFireMode") && !Input.GetButton("Fire1"))
+        {
+            ToggleFireMode();
+
+        }
+
+    }
+
+    abstract protected void Shoot();
+
 }
--- Assets/Scripts/GunProjectile.cs
+++ Assets/Scripts/GunProjectile.cs
@@ -17,38 +17,8 @@ public class GunProjectile : Gun {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && fireMode == 2)
-        {
-            nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
-        }
-        else if (Input.GetButtonDown("Fire1") && fireMode == 0)
-        {
-            Shoot();
-        }
-        else if (Input.GetButton("Fire1") && burstCounter < burstAmount && Time.time >= nextTimeToFire && fireMode == 1)
-        {
-            nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
-            burstCounter++;
-        }
-        else if (Input.GetButtonUp("Fire1") && burstCounter >= burstAmount && fireMode == 1)
-        {
-            burstCounter = 0;
-        }
 
-        if (Input.GetButtonDown("ToggleFireMode") && !Input.GetButton("Fire1"))
-        {
-            ToggleFireMode();
-
-        }
-
-
-    }
-
-    private void Shoot()
+    protected override void Shoot()
     {
         GameObject laser = Instantiate(laserPrefab,muzzle.transform.position,muzzle.transform.rotation);
         laser.GetComponent<Rigidbody>().AddForce(-laser.transform.up * 100f);
