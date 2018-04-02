abc09cc25fe7dd565d2b96622fece661e8cb0e1b
Aditya Bawankule <adityabawankule@gmail.com>
2018-04-02 02:20:56 -0500

    Create MiniMap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+    private float yPos;
+
+    void Start()
+    {
+        yPos = transform.position.y;
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        transform.position = new Vector3(player.position.x, yPos, player.position.z);
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y,0);
+	}
+}
