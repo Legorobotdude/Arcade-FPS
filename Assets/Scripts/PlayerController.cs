c8c72010e4315b441c81ee3f5c40c0602cba6fe5
Zee2 <finnnorth@gmail.com>
2018-04-14 21:20:31 -0500

    Jetpack/Tether

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float runSpeed = 10f;
     [SerializeField] private float lookSensitivity = 3f;
     [SerializeField] private float jump_force = 500f;
+    [SerializeField] private float tetherStrength = 10f;
+    [SerializeField] private float jetpackStrength = 10f;
 
-
+    
+    private Player player;
     private PlayerMotor motor;
     private CapsuleCollider playerCollider;
-
+    private Rigidbody playerRigidbody;
+    private Vector3 movementInput;
     // Use this for initialization
     void Start()
     {
         motor = GetComponent<PlayerMotor>();
         playerCollider = GetComponent<CapsuleCollider>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        player = GetComponent<Player>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -31,8 +37,10 @@ public class PlayerController : MonoBehaviour
         //Calculate movement vel as 3d vector
         float _xMovement = Input.GetAxisRaw("Horizontal");
         float _zMovement = Input.GetAxisRaw("Vertical");
-
+        float _yMovement = Input.GetAxisRaw("Jetpack");
         //Calculate movement
+
+        movementInput = new Vector3(_xMovement, _yMovement, _zMovement);
 
         Vector3 _movHorizontal = transform.right * _xMovement;
         Vector3 _movVertical = transform.forward * _zMovement;
@@ -51,7 +59,7 @@ public class PlayerController : MonoBehaviour
             _velocity = (_movHorizontal + _movVertical).normalized * speed;
         }
 
-        motor.Move(_velocity);
+        //motor.Move(_velocity);
 
         //Move the character for horizontal mouse movement
         float _yRot = Input.GetAxisRaw("Mouse X");
@@ -69,6 +77,16 @@ public class PlayerController : MonoBehaviour
 
         
 
+    }
+
+    public void SetHitAttraction(Vector3 hit){
+
+        playerRigidbody.AddForce(Vector3.Normalize(hit - playerRigidbody.position) * tetherStrength);
+    
+    }
+
+    void FixedUpdate(){
+        playerRigidbody.AddForce(Vector3.Normalize(player.camera.transform.TransformVector(movementInput)) * jetpackStrength);
     }
 
     //Check if we are on the ground
